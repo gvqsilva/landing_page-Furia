aabfef5aee8fa4c7c02e6b6de94b6b831fccd65c
Gabriel Vasquez <110639916+gvqsilva@users.noreply.github.com>
2025-05-01 21:33:20 -0300

    Atualizar o ChatSimulado.jsx
    alterando o botão de enviar

--- src/components/ChatSimulado.jsx
+++ src/components/ChatSimulado.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { FaTrophy, FaMapSigns, FaUserAlt, FaUserShield, FaSkull } from 'react-icons/fa';
+import { SlActionRedo } from "react-icons/sl";
+
 
 const PageContainer = styled.div`
   display: flex;
@@ -295,7 +297,7 @@ function ChatSimulado() {
               onChange={(e) => setInputMessage(e.target.value)}
               placeholder="Digite sua mensagem..."
             />
-            <SendButton onClick={sendMessage}>Enviar</SendButton>
+            <SendButton onClick={sendMessage}><SlActionRedo /></SendButton>
           </InputContainer>
         </ChatContainer>
 
